1c51364af493bfddda89d6ccb75b8186452bfca4
JustinPong <Justinpong0416@gmail.com>
2022-02-21 05:51:47 +0800

    new version updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,27 +3,31 @@ import './App.css';
 
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+
 import Yelp from '../../util/Yelp';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      businesses: [],
-    }
-  }
-  searchYelp(term, location, sortBy) {
-    Yelp.searchYelp(term, location, sortBy).then((businesses) => {
-      this.setState({ businesses: businesses});
-    })
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
   }
 
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
 
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar searchYelp={this.searchYelp}/>
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={this.state.businesses} />
       </div>
     );
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,33 +1,30 @@
-const apiKey = "bmaWHnJ-0UZZ5UjxqTTOGYlPQfVBMgIuuBoGFjFsFGQKZEaDGIfgv8hhX7uzUkHYdLI0XAtmOegObydjFzeChgsoSa9aRyXPcbA6-4jQ6dW-5n9qu66gMLbs_-TbYXYx";
+const apiKey = ''; // Insert API key here.
 
 const Yelp = {
-    search (term, location, sortBy) {
+  search(term, location, sortBy) {
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-        headers: {
-            Authorization: `Bearer ${apiKey}` 
-          },
-    }).then((response) => {
-        return response.json();
-    }).then((jsonResponse) => {
-        if(jsonResponse.businesses) {
-            return jsonResponse.businesses.map(((business) => {
-                console.log(business);
-                return {
-                    id: business.id,
-                    imageSrc: business.image_url,
-                    name: business.name,
-                    address: business.location.address1,
-                    city: business.location.city,
-                    state: business.location.state,
-                    zipCode: business.location.zip_Code,
-                    category: business.categories[0].title,
-                    rating: business.rating,
-                    reviewCount: business.review_count,
-                };
-            }))
-        }
-    })
-    
-}};
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
 
 export default Yelp;
